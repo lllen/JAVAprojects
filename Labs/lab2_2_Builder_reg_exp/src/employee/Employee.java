6cc27afc2b6858e91f987b009f60e948c5283ccf
rozhko <olena1q2@gmail.com>
2017-10-22 00:45:00 +0300

    Added tests for regular exp and changed EmployeeBuilder from nested class to inner class

--- Labs/lab2_2_Builder_reg_exp/src/employee/Employee.java
+++ Labs/lab2_2_Builder_reg_exp/src/employee/Employee.java
@@ -7,48 +7,32 @@ import java.util.regex.Pattern;
 
 public class Employee {
 
-    private final String firstName;
-    private final String secondName;
-    private final LocalDate dateOfBirth;
+    private  String firstName;
+    private  String secondName;
+    private  LocalDate dateOfBirth;
     private  LocalDate firstDayAtWork;
     private String phoneNumber;
     private String emailAddress;
     private String workingPosition;
     private double salary;
-    Employee(EmployeeBuilder employeeBuilder) {
+    private final String checkFirstSecondName="^[A-Z][a-z]{3,14}$";
+    private final String checkPhoneNumber="^\\+380[0-9]{9}$";
+    private final String checkEmailAddress="^[a-z]+\\w{1,10}+@(mail|[a-z]+|[a-z]+mail])\\.(com|ru|ua)$";
 
-        this.firstName = employeeBuilder.getFirstName();
-        this.secondName = employeeBuilder.getSecondName();
-        this.workingPosition = employeeBuilder.getWorkingPosition();
-        this.firstDayAtWork = employeeBuilder.getFirstDayAtWork();
-        this.dateOfBirth=employeeBuilder.getDateOfBirth();
-        this.phoneNumber=employeeBuilder.getPhoneNumber();
-        this.emailAddress=employeeBuilder.getEmailAddress();
-        this.salary=employeeBuilder.getSalary();
+
+    public static EmployeeBuilder newEmployeeBuilder() {
+        return new Employee().new EmployeeBuilder();
     }
-
-
-
-
-    // BUILDER
-    public static class EmployeeBuilder {
-
-        private String firstName;
-        private String secondName;
-        private LocalDate dateOfBirth;
-        private String workingPosition;
-        private LocalDate firstDayAtWork;
-        private String phoneNumber;
-        private String emailAddress;
-        private double salary;
+    // BUILDER (inner class)
+    public  class EmployeeBuilder {
 
         public EmployeeBuilder setSalary(double salary){
-            this.salary=salary;
+            Employee.this.salary=salary;
             return  this;
         }
         public EmployeeBuilder setFirstName(final String firstName) throws RuntimeException{
             if(checkFirstSecondName(firstName)){
-                this.firstName = firstName;
+                Employee.this.firstName = firstName;
                 return this;
             }
             throw new RuntimeException("Incorrect first name !");
@@ -56,7 +40,7 @@ public class Employee {
 
         public EmployeeBuilder setSecondName(final String secondName) throws RuntimeException{
             if(checkFirstSecondName(secondName)) {
-                this.secondName = secondName;
+                Employee.this.secondName = secondName;
                 return this;
             }
 
@@ -65,7 +49,7 @@ public class Employee {
 
         public EmployeeBuilder setPhoneNumber(String phoneNumber)throws RuntimeException{
             if(checkPhoneNumber(phoneNumber)){
-                this.phoneNumber=phoneNumber;
+                Employee.this.phoneNumber=phoneNumber;
                 return this;
             }
             throw new RuntimeException("Incorrect phone number !");
@@ -73,82 +57,29 @@ public class Employee {
 
         public EmployeeBuilder setEmailAddress(String emailAddress)throws RuntimeException{
             if(checkEmailAddress(emailAddress)){
-                this.emailAddress=emailAddress;
+                Employee.this.emailAddress=emailAddress;
                 return this;
             }
             throw new RuntimeException("Incorrect e-mail address !");
         }
 
         public EmployeeBuilder setDateOfBirth(LocalDate dateOfBirth){
-            this.dateOfBirth=dateOfBirth;
+            Employee.this.dateOfBirth=dateOfBirth;
             return this;
         }
 
-        public EmployeeBuilder setWorkingPosition(String workingPosition) {
-            this.workingPosition = workingPosition;
-            return this;
+        public EmployeeBuilder setWorkingPosition(String workingPosition){
+                Employee.this.workingPosition = workingPosition;
+                return this;
         }
 
         public EmployeeBuilder setFirstDayAtWork(LocalDate firstDayAtWork) {
-            this.firstDayAtWork = firstDayAtWork;
+            Employee.this.firstDayAtWork = firstDayAtWork;
             return this;
         }
 
-        public double getSalary(){return salary;}
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getSecondName() {
-            return secondName;
-        }
-
-        public String getWorkingPosition() {
-            return workingPosition;
-        }
-
-        public LocalDate getFirstDayAtWork() {
-            return firstDayAtWork;
-        }
-
-        public LocalDate getDateOfBirth(){return dateOfBirth;}
-
-        public String getEmailAddress(){return emailAddress;}
-
-        public String getPhoneNumber(){return phoneNumber;}
-
         public Employee build() {
-            return new Employee(this);
-        }
-
-
-        // regular exp
-        public boolean checkFirstSecondName(final String firstName){
-            String patternName="^[A-Z][a-z]{3,14}$";
-            Pattern pattern = Pattern.compile(patternName);  // [0-15] letters allowed
-            Matcher matcher = pattern.matcher(firstName);
-            if(matcher.matches())
-                return true;
-            return false;
-        }
-
-        public boolean checkPhoneNumber(String phoneNumber){
-            String patternPhoneNumber="^\\+380[0-9]{9}$";
-            Pattern pattern=Pattern.compile(patternPhoneNumber);
-            Matcher matcher=pattern.matcher(phoneNumber);
-            if(matcher.matches())
-                return true;
-            return false;
-        }
-
-        public boolean checkEmailAddress(String emailAddress){
-            String patternEmailAddress="^[a-z]{1}[a-z-0-9]{3,10}\\@[a-z]{1,6}mail\\.[a-z]{2,3}$";
-            Pattern pattern=Pattern.compile(patternEmailAddress);
-            Matcher matcher=pattern.matcher(emailAddress);
-            if(matcher.matches())
-                return true;
-            return false;
+            return  Employee.this;
         }
     }
 
@@ -212,7 +143,29 @@ public class Employee {
 
 
     // OTHERS
+    public boolean checkFirstSecondName(final String firstName){
+        Pattern pattern = Pattern.compile(checkFirstSecondName);  // [0-15] letters allowed
+        Matcher matcher = pattern.matcher(firstName);
+        if(matcher.matches())
+            return true;
+        return false;
+    }
 
+    public boolean checkPhoneNumber(String phoneNumber){
+        Pattern pattern=Pattern.compile(checkPhoneNumber);
+        Matcher matcher=pattern.matcher(phoneNumber);
+        if(matcher.matches())
+            return true;
+        return false;
+    }
+
+    public boolean checkEmailAddress(String emailAddress){
+        Pattern pattern=Pattern.compile(checkEmailAddress);
+        Matcher matcher=pattern.matcher(emailAddress);
+        if(matcher.matches())
+            return true;
+        return false;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o)
--- Labs/lab2_2_Builder_reg_exp/src/employeeTest/EmployeeTest.java
+++ Labs/lab2_2_Builder_reg_exp/src/employeeTest/EmployeeTest.java
@@ -7,71 +7,106 @@ import java.time.LocalDate;
 
 public class EmployeeTest {
     @DataProvider
-    Object[][] WorkingExperienceTimeProvider(){
-        Employee ob1 = new Employee.EmployeeBuilder()
-                .setFirstDayAtWork(LocalDate.of(2015,05,05))
+    Object[][] WorkingExperienceTimeProvider() {
+        Employee ob1 = Employee.newEmployeeBuilder()
+                .setFirstDayAtWork(LocalDate.of(2015, 05, 05))
                 .build();
-        Employee ob2=new  Employee.EmployeeBuilder()
-                .setFirstDayAtWork(LocalDate.of(2017,05,26))
+        Employee ob2 = Employee.newEmployeeBuilder()
+                .setFirstDayAtWork(LocalDate.of(2017, 05, 26))
                 .build();
-        Employee ob3=new  Employee.EmployeeBuilder()
-                .setFirstDayAtWork(LocalDate.of(2018,05,26))
+        Employee ob3 = Employee.newEmployeeBuilder()
+                .setFirstDayAtWork(LocalDate.of(2018, 05, 26))
                 .build();
-        Employee ob4=new  Employee.EmployeeBuilder()
+        Employee ob4 = Employee.newEmployeeBuilder()
                 .setFirstDayAtWork(LocalDate.now())
                 .build();
 
-        return new Object[][]{{ob1.getWorkingExperience(),2},{ob2.getWorkingExperience(),0},{ob3.getWorkingExperience(),0},{ob4.getWorkingExperience(),0}};
+        return new Object[][]{{ob1.getWorkingExperience(), 2}, {ob2.getWorkingExperience(), 0}, {ob3.getWorkingExperience(), 0}, {ob4.getWorkingExperience(), 0}};
     }
+
     @Test(dataProvider = "WorkingExperienceTimeProvider")
-    public void testCalculateWorkingExperience(long workingExperience,long expectedWorkingExperience) {
-        Assert.assertEquals(workingExperience,expectedWorkingExperience);
+    public void testCalculateWorkingExperience(long workingExperience, long expectedWorkingExperience) {
+        Assert.assertEquals(workingExperience, expectedWorkingExperience);
     }
 
     @DataProvider
-    Object[][] AgeProvider(){
-        Employee ob1=new Employee.EmployeeBuilder()
-                .setDateOfBirth(LocalDate.of(1976,03,8))
+    Object[][] AgeProvider() {
+        Employee ob1 = Employee.newEmployeeBuilder()
+                .setDateOfBirth(LocalDate.of(1976, 03, 8))
                 .build();
-        Employee ob2=new Employee.EmployeeBuilder()
+        Employee ob2 = Employee.newEmployeeBuilder()
                 .setDateOfBirth(LocalDate.now())
                 .build();
-        Employee ob3=new Employee.EmployeeBuilder()
-                .setDateOfBirth(LocalDate.of(2018,03,8))
+        Employee ob3 = Employee.newEmployeeBuilder()
+                .setDateOfBirth(LocalDate.of(2018, 03, 8))
                 .build();
 
-        return new Object[][]{{ob1.getAge(),41},{ob2.getAge(),0},{ob3.getAge(),0}};
+        return new Object[][]{{ob1.getAge(), 41}, {ob2.getAge(), 0}, {ob3.getAge(), 0}};
     }
 
     @Test(dataProvider = "AgeProvider")
-    public void testCalculateAge(long dateOfBirth, long expectedDateOfBirth){
-        Assert.assertEquals(dateOfBirth,expectedDateOfBirth);
+    public void testCalculateAge(long dateOfBirth, long expectedDateOfBirth) {
+        Assert.assertEquals(dateOfBirth, expectedDateOfBirth);
     }
 
     @DataProvider
-    Object[][] nameRegExpProvider(){
-        String name1="dima";
-        String name2="Dima";
-        String name3="DimaDimaDimaDifergrfg";
-        String name4="Lisa lisa";
+    Object[][] nameRegExpProvider() {
+        String name1 = "dima";
+        String name2 = "Dima";
+        String name3 = "DimaDimaDimaDifergrfg";
+        String name4 = "Lisa lisa";
 
-        Employee empl1=new Employee.EmployeeBuilder()
-                .setFirstName(name1)
-                .build();
-        Employee empl2=new Employee.EmployeeBuilder()
-                .setFirstName(name2)
-                .build();
-        Employee empl3=new Employee.EmployeeBuilder()
-                .setFirstName(name3)
-                .build();
-        Employee empl4=new Employee.EmployeeBuilder()
-                .setFirstName(name4)
+        Employee empl1 = Employee.newEmployeeBuilder()
                 .build();
 
-        return new Object[][]{{},{},{},{}};
+        return new Object[][]{{empl1.checkFirstSecondName(name1), false}, {empl1.checkFirstSecondName(name2), true}, {empl1.checkFirstSecondName(name3), false}, {empl1.checkFirstSecondName(name4), false}};
     }
+
     @Test(dataProvider = "nameRegExpProvider")
-    public void nameRegExpTest(String name,boolean flag){
-        Assert.assertEquals();
+    public void nameRegExpTest(boolean rezult, boolean expected_rezult) {
+        Assert.assertEquals(rezult, expected_rezult);
     }
+
+    @DataProvider
+    Object[][] phoneNumberRegExpProvider() {
+        String phoneNumber1 = "+38000"; // not enough numbers
+        String phoneNumber2 = "+380992776367"; // everuthing is ok
+        String phoneNumber3 = "+380 99 2 7 7 6 3 6 7"; // unwanted space
+        String phoneNumber4 = "+38012345678910"; // too much numbers
+        String phoneNumber5 = "+380/9927763"; // unwanted symbol
+
+        Employee empl1 = Employee.newEmployeeBuilder()
+                .build();
+
+        return new Object[][]{{empl1.checkPhoneNumber(phoneNumber1), false}, {empl1.checkPhoneNumber(phoneNumber2), true}, {empl1.checkPhoneNumber(phoneNumber3), false}, {empl1.checkPhoneNumber(phoneNumber4), false}, {empl1.checkPhoneNumber(phoneNumber5), false}};
+    }
+
+    @Test(dataProvider = "phoneNumberRegExpProvider")
+    public void phoneNumberRegExpTest(boolean rezult, boolean expected_rezult) {
+        Assert.assertEquals(rezult, expected_rezult);
+    }
+
+    @DataProvider
+    Object[][] emailAddressRegExpProvider() {
+        String email1 = "olena@11.11";
+        String email2 = "oLena@gmail.com";
+        String email3 = "1olena@mail.com";
+        String email4 = "olen1a11d3@ya.ua";
+        String email5 = "olE1245i@mail.ru";
+        String email6 = "olena@@mail.ru";
+        String email7 = "olena@gmailcom";
+
+        Employee empl = Employee.newEmployeeBuilder()
+                .build();
+        return new Object[][]{{empl.checkEmailAddress(email1), false}, {empl.checkEmailAddress(email2), true}, {empl.checkEmailAddress(email3), false},
+                {empl.checkEmailAddress(email4), true}, {empl.checkEmailAddress(email5), true}, {empl.checkEmailAddress(email6), false},
+        {empl.checkEmailAddress(email7), false}};
+    }
+
+    @Test(dataProvider = "emailAddressRegExpProvider")
+    public void emailAddressRegExpTest(boolean rezult,boolean expected_rezult){
+        Assert.assertEquals(rezult,expected_rezult);
+    }
+
+
 }
