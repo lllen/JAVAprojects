64b511ba6fe4fafb9e1964e7eca450e7534beb3f
rozhko <olena1q2@gmail.com>
2017-10-26 23:25:46 +0300

    some changes

--- Labs/lab2_3_Agregation/src/mainTemp.java
+++ Labs/lab2_3_Agregation/src/mainTemp.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 public class mainTemp {
     public static void main(String [] args) throws IOException {
 
-        File file;
+     /*   File file;
         FileOutputStream fos = null;
         file = new File("E:\\JAVA_PROJ\\Labs\\lab2_3_Agregation");
         fos = new FileOutputStream(file);
 
-        fos.write(5);
+        fos.write(5);*/
     }
 }
